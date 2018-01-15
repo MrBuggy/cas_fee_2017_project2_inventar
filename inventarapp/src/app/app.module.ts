8a18873cc52cd544e5346d71f8decf435862f1d3
Srdan Turudic <srdan.turudic@namics.com>
2018-01-15 23:25:16 +0100

    INVENTAR-0002 [WIP]

--- inventarapp/src/app/app.module.ts
+++ inventarapp/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './components/molecules/test/test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    JsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
