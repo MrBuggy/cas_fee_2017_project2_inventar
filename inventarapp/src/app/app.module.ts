b42c02c243d45adba9582cb09f42c4759f82ab24
Srdan Turudic <srdan.turudic@namics.com>
2018-03-06 10:29:42 +0100

    WIP

--- inventarapp/src/app/app.module.ts
+++ inventarapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 /* COMPONENTS */
 import { AppComponent } from './app.component';
@@ -8,18 +9,39 @@ import { AppComponent } from './app.component';
 /* SERVICES */
 import { NavigationService } from './services/navigation.service';
 import { NavigationComponent } from './components/molecules/navigation/navigation.component';
-import { HeaderComponent } from './components/molecules/header/header.component';
+import { MainHeaderComponent } from './components/molecules/main-header/main-header.component';
+import { ProfileComponent } from './components/molecules/profile/profile.component';
+import { InventoryComponent } from './components/molecules/inventory/inventory.component';
+import { SearchComponent } from './components/molecules/search/search.component';
+import { SocietyComponent } from './components/molecules/society/society.component';
+
+// Route Configuration
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/profile', pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'society', component: SocietyComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HeaderComponent
+    MainHeaderComponent,
+    ProfileComponent,
+    InventoryComponent,
+    SearchComponent,
+    SocietyComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [NavigationService],
   bootstrap: [AppComponent]
